247e452c702fc7cdc5d56da930bb89fe82c2670b
Maurizio Pratesi <pratesi.maurizio@gmail.com>
2013-08-24 12:48:40 +0200

    Versione funzionante

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\bin\AndroidManifest.xml : \
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,12 @@
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\bin\classes.dex : \
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\bin\classes\com\example\accel\BuildConfig.class \
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\bin\classes\com\example\accel\exampleAccelerometer.class \
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\bin\classes\com\example\accel\R$attr.class \
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\bin\classes\com\example\accel\R$drawable.class \
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\bin\classes\com\example\accel\R$layout.class \
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\bin\classes\com\example\accel\R$string.class \
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\bin\classes\com\example\accel\R$xml.class \
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\bin\classes\com\example\accel\R.class \
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\bin\classes\org\apache\cordova\devicemotion\AccelListener$1.class \
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\bin\classes\org\apache\cordova\devicemotion\AccelListener.class \
+C:\Users\Maurizio\Documents\GitHub\exampleAccel\platforms\android\bin\dexedLibs\cordova-3.0.0-76c2ab8c232cc0454a85aaaf55a99b87.jar \
